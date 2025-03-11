62f19be510605acb2044cbcd3346abeb3ea98b70
AMOL SARGAR <67868005+amolsargar6@users.noreply.github.com>
2025-03-11 18:56:38 +0000

    conversions

---
+++ 01_BASICS/03_conversions.js
@@ -0,0 +1,19 @@
+let score = "99a";
+console.log(typeof(score));
+
+//converting in number
+let scoreInNum = Number(score);
+console.log(typeof(scoreInNum));
+console.log(scoreInNum);
+
+console.log(typeof(NaN));
+
+/*
+
+"33" -> 33
+"33ab" -> NaN
+true -> 1; false->0
+null -> 0
+undefined -> NaN
+
+*/
