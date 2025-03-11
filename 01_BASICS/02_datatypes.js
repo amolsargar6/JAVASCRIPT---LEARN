ee6782daf792149d2da462309ec4c19ab6d5946e
AMOL SARGAR <67868005+amolsargar6@users.noreply.github.com>
2025-03-11 18:55:17 +0000

    datatypes

---
+++ 01_BASICS/02_datatypes.js
@@ -0,0 +1,37 @@
+"use strict"; //this treats JS code as newer version
+
+//alert(3+3) // this dont work in nodejs env as alert works only on browser
+
+console.log("Amol");
+
+
+/*******************************
+
+-----------DATA TYPES-----------
+
+--premitive data type--
+
+number -> 2^53
+bigint
+string -> "this is string"
+boolean -> true/false
+
+null -> standalone value, representation of empty value
+undefined -> value is not assigned
+
+symbol -> unique
+
+
+--object--
+
+
+********************************/
+
+console.log(typeof undefined);
+console.log(typeof null);
+
+
+
+
+
+
